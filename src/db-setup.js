afb1d6df155960175620388af105715a2a3addc7
h0wter <91832203+h0wter@users.noreply.github.com>
2023-09-06 20:15:08 +0300

    + setup db connection

---
+++ src/db-setup.js
@@ -0,0 +1,17 @@
+import db from "./db.js";
+import User from "./models/User.js";
+import Profile from "./models/Profile.js";
+
+console.log("dbsetup");
+
+export const syncDatabase = async () => {
+  try {
+    await db.sync();
+    await User.sync();
+    await Profile.sync();
+    await db.authenticate();
+    console.log("Tables synced successfully");
+  } catch (error) {
+    console.error("Error syncing tables:", error);
+  }
+};
